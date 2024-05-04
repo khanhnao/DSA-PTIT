f6e6588fe16f8d9c41e3eeca421a16159db8217e
Tran Nguyen Khanh <trannguyenkhanh.tnk2004@gmail.com>
2024-05-04 10:52:28 +0700

    DSA03005 CHIA MẢNG THÀNH HAI MẢNG COM CÓ TỔNG LỚN NHẤT

---
+++ DSA3005.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    int t; cin >> t;
+    while(t--) {
+        int n, k;
+        cin >> n >> k;
+        int a[100];
+        for (int i = 0; i < n; i++) {
+        	cin >> a[i];
+		}
+		sort(a, a+n);
+		int sum1 = 0, sum2 = 0;
+		k = min(k, n - k);
+		for (int i = 0; i < k; i++) {
+			sum1 += a[i];
+		}
+		for (int i = k; i < n; i++) {
+			sum2 += a[i];
+		}
+		cout << abs(sum1 - sum2) << endl;
+    }
+}

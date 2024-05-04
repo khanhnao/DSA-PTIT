a049d961441083fd41ba305f8d7c21d29d3f2404
Tran Nguyen Khanh <trannguyenkhanh.tnk2004@gmail.com>
2024-05-04 10:40:53 +0700

    DSA01021 TỔ HỢP TIẾP THEO

---
+++ DSA1021.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<set>
+using namespace std;
+
+int n, k, a[100], ok;
+set <int> se;
+
+void sinh() {
+	int i = k;
+	while (i >= 1 && a[i] == n - k + i) {
+		i--;
+	}
+	if (i == 0) {
+		cout << k;
+	} else {
+		a[i]++;
+		for (int j = i + 1; j <= k; j++) {
+			a[j] = a[j - 1] + 1;
+		}
+		int cnt = 0;
+		for (int i = 1; i <= k; i++) {
+			if (se.find(a[i]) == se.end()) {
+				cnt++;
+			}
+		}
+		cout << cnt;
+	}
+}
+
+int main() {
+	int t; cin >> t;
+	while(t--) {
+		cin >> n >> k;
+		for (int i = 1; i <= k; i++) {
+			cin >> a[i];
+			se.insert(a[i]);
+		}
+		sinh();
+		cout << endl;
+		se.clear();
+	}
+}

23aede13dda45a4898441f16647592ef0c45b44b
Tran Nguyen Khanh <trannguyenkhanh.tnk2004@gmail.com>
2024-05-04 11:24:43 +0700

    DSA03018 SỐ MAY MẮN

---
+++ DSA3018.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t; cin >> t;
+	while(t--) {
+		int sum; cin >> sum;
+		int d4 = -1, d7;
+		for (d7 = sum / 7; d7 >= 0; d7--) {
+			if ((sum - d7 * 7) % 4 == 0) {
+				d4 = (sum - d7 * 7) / 4;
+				break;
+			}
+		}
+		if (d4 != -1) {
+			for (int i = 0; i < d4; i++) {
+				cout << 4;
+			}
+			for (int i = 0; i < d7; i++) {
+				cout << 7;
+			}
+		} else {
+			cout << -1;
+		}
+		cout << endl;
+	}
+}

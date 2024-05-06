ab0f82da9766f9ef7e843a5e3d9bfcfab1321c27
Tran Nguyen Khanh <trannguyenkhanh.tnk2004@gmail.com>
2024-05-06 23:43:25 +0700

    DSA04011 TÍCH HAI SỐ NHỊ PHÂN

---
+++ DSA4011.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int k;
+string a, b;
+
+long long xuly(string s, int k) {
+	long long sum = 0;
+	for (int i = 0; i < s.length(); i++) {
+		sum = sum * k + s[i] - '0';
+	}
+	return sum;
+}
+
+void xuly2() {
+	long long s = xuly(a, 2) * xuly(b, 2);
+	cout << s;
+	cout << endl;
+}
+
+main() {
+	int t;
+	cin >> t;
+	while (t--) {
+		cin >> a >> b;
+		xuly2();
+	}
+}

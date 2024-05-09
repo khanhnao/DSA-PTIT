124b6a414ffae9989786d5eaa8114325eb64b815
Tran Nguyen Khanh <trannguyenkhanh.tnk2004@gmail.com>
2024-05-09 13:13:15 +0700

    DSA05008 DÃY CON CÓ TỔNG BẰNG S

---
+++ DSA5008.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool f[205][40005];
+
+int main() {
+	int t; cin >> t;
+	while(t--) {
+		int n, s;
+		cin >> n >> s;
+		int a[n + 1];
+		for (int i = 1; i <= n; i++) {
+			cin >> a[i];
+		}
+		for (int i = 0; i <= n; i++) {
+			f[i][0] = true;
+		}
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= s; j++) {
+				if (j >= a[i]) {
+					f[i][j] = f[i - 1][j - a[i]] || f[i - 1][j];
+				} else {
+					f[i][j] = f[i - 1][j];
+				}
+			}
+		}
+		if (f[n][s]) cout << "YES\n";
+		else cout << "NO\n";
+	}
+}

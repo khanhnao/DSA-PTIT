15ed0295fd73f20e63adf61a7da92d7ed697db5d
Tran Nguyen Khanh <trannguyenkhanh.tnk2004@gmail.com>
2024-05-22 08:34:40 +0700

    DSA02012 DI CHUYỂN TRONG MA TRẬN

---
+++ DSA2012.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t; cin >> t;
+	while(t--) {
+		int n, m, x;
+        cin >> n >> m;
+        int a[n + 1][m + 1] = {};
+        a[1][0] = 1;
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= m; j++)
+            {
+                cin >> x;
+                a[i][j] = a[i - 1][j] + a[i][j - 1];
+            }
+        }
+        cout << a[n][m] << endl;
+	}
+}

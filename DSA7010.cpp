d30930a25b11c77e48d36c2ffb572ce751249551
Tran Nguyen Khanh <trannguyenkhanh.tnk2004@gmail.com>
2024-05-25 14:00:02 +0700

    DSA07010 BIẾN ĐỔI TIỀN TỐ - HẬU TỐ

---
+++ DSA7010.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+	int t; cin >> t;
+	while(t--) {
+		string s; cin >> s;
+		stack<string> st;
+		for (int i = s.size() - 1; i >= 0; i--) {
+			if (isalpha(s[i])) {
+				st.push(string(1, s[i]));
+			} else {
+				string fi = st.top(); st.pop();
+				string se = st.top(); st.pop();
+				string tmp = fi + se + s[i];
+				st.push(tmp);
+			}
+		} 
+		cout << st.top() << endl;
+	}
+}

75b9c6007047f0fa9851c59020b75f5dcabe2be6
Tran Nguyen Khanh <trannguyenkhanh.tnk2004@gmail.com>
2024-05-25 14:01:13 +0700

    DSA07012 BIẾN ĐỔI HẬU TỐ - TRUNG TỐ

---
+++ DSA7012.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+	int t; cin >> t;
+	while(t--) {
+		string s; cin >> s;
+		string res = "";
+		stack<string> st;
+		for (int i = 0; i < s.size(); i++) {
+			if(isalpha(s[i])) {
+				st.push(string(1, s[i]));
+			} else {
+				string fi = st.top(); st.pop();
+				string se = st.top(); st.pop();
+				string tmp = '(' + se + s[i] + fi + ')';
+				st.push(tmp);
+			}
+		}
+		cout << st.top() << endl;
+	}
+}

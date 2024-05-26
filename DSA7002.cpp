2fd7779505971449cdc86473f1476eed287d9187
Tran Nguyen Khanh <trannguyenkhanh.tnk2004@gmail.com>
2024-05-26 15:27:45 +0700

    DSA07002 NGĂN XẾP 2

---
+++ DSA7002.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+	int t, n;
+    string s;
+    cin >> t;
+    stack<int> st;
+	while(t--) {
+		cin >> s;
+		if(s == "PUSH") {
+			cin >> n;
+			st.push(n);
+		} else if(s == "POP") {
+			if(st.size()) {
+				st.pop();
+			}
+		} else {
+			if(st.size()) {
+				cout << st.top() << endl;
+			} else cout << "NONE\n";
+		}
+	}
+}
